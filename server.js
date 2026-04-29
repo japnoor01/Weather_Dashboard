e5765c3893bb8ff4fcc7af4dd839780db72d912d
japnoors35@gmail.com <japnoors35@gmail.com>
2026-04-29 22:42:35 +0530

    cs

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Server running");
+});
+
+
+
+
 // --- 1. IMPORT DEPENDENCIES ---
 require('dotenv').config({ path: '.env' });
 const express = require('express');
